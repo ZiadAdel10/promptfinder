ec4ff6941370f7726f0abf0df3ece8c9b5e0d021
Ziad Adel Ismail <ziad.adel.mahmoud@gmail.com>
2023-05-06 13:48:23 +0300

    initial commit

---
+++ utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "PromptFinder",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
